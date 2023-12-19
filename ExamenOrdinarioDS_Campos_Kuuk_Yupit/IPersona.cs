f0d0df97a82a4a179ce7ad9068d9bdc0afbd5d3c
RobertoYB <146978884+RobertoYB@users.noreply.github.com>
2023-12-19 08:46:07 -0600

    Actualizar IPersona
    Actualice los nombres de las variables al español

--- ExamenOrdinarioDS_Campos_Kuuk_Yupit/IPersona.cs
+++ ExamenOrdinarioDS_Campos_Kuuk_Yupit/IPersona.cs
@@ -8,10 +8,10 @@ namespace ExamenOrdinarioDS_Campos_Kuuk_Yupit
 {
     public interface IPersona    //Creación de la interfaz persona
     {
-        int id { get; set; }    //Con id se refiere a un identificador numérico
-        string name { get; set; }
+        int Identificador { get; set; }    //Con id se refiere a un identificador numérico
+        string Nombre { get; set; }
         void ObtenerMascotas();
-        void ObtenerMascotasId();   //Obtener mascotas por medio del identificador numérico
+        void ObtenerMascotasIdentificador();   //Obtener mascotas por medio del identificador numérico
         void AcariciarMascota();
         void Acariciar(); 
         void AgregarMascota();
