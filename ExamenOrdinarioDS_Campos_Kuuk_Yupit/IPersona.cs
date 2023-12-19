6af2284156547bbdabf58f5fe3700c01f3b8de59
RobertoYB <ryupit26@gmail.com>
2023-12-19 09:45:07 -0600

    Multiples cambios en Clase Persona
    Modifique la Clase Persona.
    Reincorporé la Interfaz IPersona porque se borró por error.

---
+++ ExamenOrdinarioDS_Campos_Kuuk_Yupit/IPersona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenOrdinarioDS_Campos_Kuuk_Yupit
+{
+    public interface IPersona    //Creación de la interfaz persona
+    {
+        int Identificador { get; set; }    //Con id se refiere a un identificador numérico
+        string Nombre { get; set; }
+
+        private List<Persona> Personas { get; set; } //Declaración de lista de personas 
+        void ObtenerMascotas();
+        void ObtenerMascotasIdentificador();   //Obtener mascotas por medio del identificador numérico
+        void AcariciarMascota();
+        void Acariciar();
+        void AgregarMascota();
+    }
+}
---
+++ ExamenOrdinarioDS_Campos_Kuuk_Yupit/Persona.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ExamenOrdinarioDS_Campos_Kuuk_Yupit
+{
+    public class Persona : IPersona   //Clase para el objeto persona
+    {
+        public string Nombre() 
+        {
+            
+        }
+
+        public int Identificador { get; set; }
+        {
+
+        }
+
+        public string Nombre { get; set; }
+
+
+
+        void ObtenerMascotas()
+        {
+
+        }
+
+        void ObtenerMascotasIdentificador()  //Obtener mascotas por medio del identificador numérico
+        {
+
+        }
+
+        void AcariciarMascota()
+        {
+            //Es lo mismo que en el acariciar solo que a todas las mascotas que posea en la lista MascotaS
+        }
+
+        void Acariciar()
+        {
+            /*
+             if(condicion de acariceable = true) {
+                Console.WriteLine($"{Persona} acaricia a {Mascota}");
+            } else {
+                Console.WriteLine($"{Persona} no puede acariciar a {Mascota}");
+            }
+             */
+        }
+
+        void AgregarMascota()
+        {
+            Mascotas.Add;
+        }
+    }
+}
