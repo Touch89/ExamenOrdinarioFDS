bb9279298ce7e324482d4dd8415899c9f5bd1f40
RobertoYB <146978884+RobertoYB@users.noreply.github.com>
2023-12-19 11:25:45 -0600

    Agregar mi clase Persona con su interfaz
    Estoy subiendo mi clase e interfaz al main.

---
+++ ExamenOrdinarioDS_Campos_Kuuk_Yupit/IPersona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenOrdinarioDS_Campos_Kuuk_Yupit
+{
+    public interface IPersona    //Creación de la interfaz persona
+    {
+        int Identificador { get; set; }    //Con id se refiere a un identificador numérico
+        string Nombre { get; set; }
+        void ObtenerMascotas();
+        void ObtenerMascotasIdentificador();   //Obtener mascotas por medio del identificador numérico
+        void AcariciarMascota();
+        void Acariciar(); 
+        void AgregarMascota();
+    }
+}
---
+++ ExamenOrdinarioDS_Campos_Kuuk_Yupit/Persona.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenOrdinarioDS_Campos_Kuuk_Yupit
+{
+    public class Persona //Clase del objeto Persona
+    {
+        int Identificador { get; set; }    //Con id se refiere a un identificador numérico
+
+        private string _nombre;
+        string Nombre { get; set; }
+
+        void ObtenerMascotas()
+        {
+
+        }
+
+        void ObtenerMascotasIdentificador()
+        {
+
+        }
+
+        void AcariciarMascota()
+        {
+
+        }
+
+        void Acariciar()
+        {
+
+        }
+
+        void AgregarMascota()
+        {
+
+        }
+    }
+}
