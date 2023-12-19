437fa025e239bae0c8aad72a25cac4b3e57a727f
RobertoYB <146978884+RobertoYB@users.noreply.github.com>
2023-12-19 11:33:24 -0600

    Actualización Clase Persona
    Cambie los datos que estaban mal de una version vieja a la nueva.

--- ExamenOrdinarioDS_Campos_Kuuk_Yupit/Persona.cs
+++ ExamenOrdinarioDS_Campos_Kuuk_Yupit/Persona.cs
@@ -1,41 +1,97 @@
-﻿using System;
+using ExamenOrdinarioDS_Campos_Kuuk_Yupit;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExamenOrdinarioDS_Campos_Kuuk_Yupit
 {
-    public class Persona //Clase del objeto Persona
+    public class Persona : IPersona   //Clase para el objeto persona
     {
-        int Identificador { get; set; }    //Con id se refiere a un identificador numérico
+        private int identificador;  
+        private string nombre;
+        private List<Mascota> = new list <mascotas>; //Creacion lista mascota 
 
-        private string _nombre;
-        string Nombre { get; set; }
+        public int Identificador { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Nombre { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
-        void ObtenerMascotas()
+        public Persona(int identificador, string nombre)
         {
-
+            this.identificador = identificador;
+            this.nombre = nombre;
+            this.mascotas = new List<Mascota>();
         }
 
-        void ObtenerMascotasIdentificador()
+        public int ObtenerIdentificador()   //Funcion para obtener el identificador de la mascota
         {
-
+            return this.identificador;
         }
 
-        void AcariciarMascota()
+        public string ObtenerNombre()   //Funcion para obtener el nombre de la mascota
         {
-
+            return this.nombre;
         }
 
-        void Acariciar()
+        public List<Mascota> ObtenerMascotas()  //Funcion para obtener las mascotas
         {
-
+            return this.mascotas;
         }
 
-        void AgregarMascota()
+        public Mascota ObtenerMascotaPorIdentificador(int identificador)
         {
+            foreach (var mascota in this.mascotas)
+            {
+                if (mascota.ObtenerIdentificador() == identificador)
+                {
+                    return mascota;
+                }
+            }
 
+            return null;
+        }
+
+        public void AcariciarMascota(Mascota mascota)
+        {
+            Console.WriteLine($"{this.nombre} acaricia a {mascota.nombre}");
+        }
+
+
+        public void Acariciar()
+        {
+            string ResponderACaricia = Console.ReadLine();
+            if (ResponderACaricia == "Si")
+            {
+                foreach (var mascota in this.mascotas)
+                {
+                    if (mascota.IsAcariciable())
+                    {
+                        Console.WriteLine($"{this.nombre} acaricia a {mascota.nombre}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{this.nombre} no puede acariciar a {mascota.nombre}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{this.nombre} no acaricia a ninguna mascota");
+            }
+        }
+
+        public void AgregarMascota(Mascota mascota)
+        {
+            this.mascotas.Add(mascota);
+        }
+
+        public void ObtenerMascotas(List<Persona> personas)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ObtenerMascotasIdentificador(int identificador)
+        {
+            throw new NotImplementedException();
         }
     }
 }
+
+
